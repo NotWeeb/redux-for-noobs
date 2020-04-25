8cc9318c1c6f5554801a5e8164974a9437d5f9aa
NotWeeb <kyrolites@gmail.com>
2020-04-25 13:44:56 +0800

    add typings

---
+++ typings/index.types.ts
@@ -0,0 +1,20 @@
+
+
+declare module 'redux-for-noobs' {
+
+  import { Action, Store, applyMiddleware } from 'redux'
+  import { Provider } from 'react-redux'
+
+  enum OPTIONS {
+    STRICT_TYPES,
+    STRICT_NAMING
+  }
+
+  function Provider()
+  function useStore(): object
+  function simpleStore(defaultState: object, options: OPTIONS[], middleware: any): Store<Object, Action>
+
+
+  export { Provider, applyMiddleware, useStore, simpleStore, OPTIONS }
+
+}
